859deaff3a22539164c6bdd29df7fbfed4151548
Anuja Gunasinghe <a.r.gunasinghe@gmail.com>
2024-12-18 00:09:36 +0530

    changes

--- Kit.mjs
+++ Kit.mjs
@@ -1,22 +1,35 @@
 import path from "path";
 import fs from "fs/promises";
+import crypto from "crypto";
 
 class Kit {
+  constructor(repoPath = ".") {
+    this.repoPath = path.join(repoPath, ".kit");
+    this.objectsPath = path.join(this.repoPath, "objects");
+    this.headPath = path.join(this.repoPath, "HEAD");
+    this.indexPath = path.join(this.repoPath, "index");
+    this.init();
+  }
 
-    constructor(repoPath = ".") {
-        this.repoPath = path.join(repoPath, ".kit");
-        this.objectsPath = path.join(this.repoPath, "objects");
-        this.headPath = path.join(this.repoPath, "HEAD");
-        this.indexPath = path.join(this.repoPath, "index");
-        this.init();
+  async init() {
+    await fs.mkdir(this.objectsPath, { recursive: true });
+    try {
+      await fs.writeFile(this.headPath, "", { flag: "wx" });
+      await fs.writeFile(this.indexPath, JSON.stringify([]), { flag: "wx" });
+    } catch (error) {
+      console.log("Kit already initialized");
     }
+  }
 
-    async init() {
-        await fs.mkdir(this.objectsPath, {recursive: true});
-        try {
-                await fs.writeFile(this.headPath, '', {flag: 'wx'});
-        } catch (error) {
-                console.log("Kit already initialized");
-        }
-    }
+  hashObject(content) {
+    return crypto.createHash("sha1").update(content, "utf-8").digest("hex");
+  }
+
+  async add(fileToBeAdded) {
+    const fileData = await fs.readFile(fileToBeAdded, { encoding: "utf-8" });
+    const fileHash = this.hashObject(fileData);
+    console.log(fileHash);
+  }
 }
+
+const kit = new Kit();
