c31e6a84c5ba0e07bd32c546c5f0b32c7015126d
Anuja Gunasinghe <a.r.gunasinghe@gmail.com>
2024-12-19 13:06:21 +0530

    commit() finished

--- Kit.mjs
+++ Kit.mjs
@@ -31,10 +31,50 @@ class Kit {
     console.log(fileHash);
     const newFileHashedObjectPath = path.join(this.objectsPath, fileHash);
     await fs.writeFile(newFileHashedObjectPath, fileData);
-    // TODO: Add the file to the staging area
+    await this.updateStagingArea(fileToBeAdded, fileHash);
     console.log(`Added ${fileToBeAdded}`);
+  }
+
+  async updateStagingArea(filePath, fileHash) {
+    const index = JSON.parse(
+      await fs.readFile(this.indexPath, { encoding: "utf-8" })
+    );
+    index.push({ path: filePath, hash: fileHash });
+    await fs.writeFile(this.indexPath, JSON.stringify(index));
+  }
+
+  async commit(message) {
+    const index = JSON.parse(
+      await fs.readFile(this.indexPath, { encoding: "utf-8" })
+    );
+    const parentCommit = await this.getCurrentHead();
+
+    const commitData = {
+      timeStamp: new Date().toISOString(),
+      message,
+      files: index,
+      parent: parentCommit,
+    };
+
+    const commitHash = this.hashObject(JSON.stringify(commitData));
+    const commitPath = path.join(this.objectsPath, commitHash);
+    await fs.writeFile(commitPath, JSON.stringify(commitData));
+    await fs.writeFile(this.headPath, commitHash);
+    await fs.writeFile(this.indexPath, JSON.stringify([]));
+    console.log(`Commit successsful: ${commitHash}`);
+  }
+
+  async getCurrentHead() {
+    try {
+      return await fs.readFile(this.headPath, { encoding: "utf-8" });
+    } catch (error) {
+      return null;
+    }
   }
 }
 
-const kit = new Kit();
-kit.add("test.txt");
+(async () => {
+  const kit = new Kit();
+  await kit.add("test.txt");
+  await kit.commit("Initial commit");
+})();
